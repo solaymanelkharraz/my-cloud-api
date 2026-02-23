c6e7a3b05eb9363eec6a1bfede2806a7ac04cd92
Soulayman Elkharraz <soulaymanelkharraz2006@gmail.com>
2026-02-23 09:40:46 +0000

    first commit

--- api/index.js
+++ api/index.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { success } = require("../functions"); // Note the ../ to go back one folder
+const { success } = require("../functions"); // Go up one folder to find functions.js
 
 const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-// CORS headers (Very important for the View to talk to the API)
+// IMPORTANT: This allows your HTML/React view to talk to this API
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+// The Sum route
 app.post("/api/v1/somme", (req, res) => {
-    let n1 = req.body.n1;
-    let n2 = req.body.n2;
-    let result = Number(n1) + Number(n2);
-    res.json(success("La somme est: " + result));
+    const n1 = req.body.n1;
+    const n2 = req.body.n2;
+    const r = Number(n1) + Number(n2);
+    res.json(success("La somme de " + n1 + " et " + n2 + " est : " + r));
 });
 
-module.exports = app; // Vercel needs this line!
+module.exports = app; // Required for Vercel
