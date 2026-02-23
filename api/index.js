9000442f19f107188fa6775761d8fe5dd42d5f44
Soulayman Elkharraz <soulaymanelkharraz2006@gmail.com>
2026-02-23 09:48:40 +0000

    first commit

--- api/index.js
+++ api/index.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { success } = require("../functions"); // Go up one folder to find functions.js
+const { success } = require("../functions");
 
 const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// IMPORTANT: This allows your HTML/React view to talk to this API
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// The Sum route
-app.post("/api/v1/somme", (req, res) => {
-    const n1 = req.body.n1;
-    const n2 = req.body.n2;
-    const r = Number(n1) + Number(n2);
-    res.json(success("La somme de " + n1 + " et " + n2 + " est : " + r));
+// Create the router exactly as shown in the Vercel PDF
+const CalculRouter = express.Router();
+
+CalculRouter.route("/somme").post((req, res) => {
+    let n1 = req.body.n1;
+    let n2 = req.body.n2;
+    let r = Number(n1) + Number(n2);
+    res.json(success("la somme de " + n1 + " et " + n2 + " est: " + r));
 });
 
-module.exports = app; // Required for Vercel
+// IMPORTANT: Link the router to the path the HTML uses
+app.use("/api/v1", CalculRouter);
+
+module.exports = app;
